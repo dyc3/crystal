5cfaed596e9e9cd6db0aaacca3a9a9735ba542d6
Carson McManus <carson.mcmanus1@gmail.com>
2017-01-17 18:56:38 -0500

    added event hooks to BaseSpeechRecognizer

--- speech/base.py
+++ speech/base.py
@@ -1,8 +1,11 @@
+from eventhook import EventHook
 
 class BaseSpeechRecognizer(object):
 	"""docstring for BaseSpeechRecognizer."""
 	def __init__(self):
 		super(BaseSpeechRecognizer, self).__init__()
+		self.onSpeech = EventHook() # called while speaking, but not finished with request (only usable with speech streaming)
+		self.onFinish = EventHook() # called when speaking is finished
 
 	def Start():
 		pass
