bf93b27d34a2de90fe63f85e3f7fb3c8ee2ad59b
Carson McManus <carson.mcmanus1@gmail.com>
2017-01-17 18:25:15 -0500

    replaced hard coded training data with training data from file

--- main.py
+++ main.py
@@ -3,20 +3,13 @@ import numpy
 import spacy
 from sklearn.metrics import accuracy_score
 import classifier
+from datautil import DataUtil
 
 print("Loading...")
 nlp = spacy.load('en')
 cmdClassifier = classifier.CommandClassifier(nlp)
 
-train = ["what time is it", "what is today's date", "what's the time", "what time is it in eastern standard time",
-		"is today friday", "give me the date", "what day of the week is it", "what time is it on the west coast",
-		"is tomorrow monday", "what year is it", "what's the date", "is today tuesday", "is today wednesday",
-		"what day of the month is it", "what month is it", "what's the current military time", "is it still morning",
-		"pause the video", "play media", "resume playback", "suspend playback", "stop the video", "play video",
-		"stop playback", "stop playing music", "set volume to 100%"]
-labelsTrain = ["time", "date", "time", "time", "date", "date", "date", "time", "date", "date", "date", "date", "date",
-		"date", "date", "time", "time", "media-pause", "media-play", "media-play", "media-pause", "media-pause", "media-play",
-		"media-pause", "media-pause", "volume-set"]
+train, labelsTrain = DataUtil.loadTrainingData("training.txt")
 
 test = ["give me the time", "is today monday", "give me the date", "what time is it on the east coast in military time",
 		"what time is it in pacific standard time", "pause music", "play the video", "stop playing that"]
