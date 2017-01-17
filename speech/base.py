1dafaaa1c35951df583169105e79683a9b7dabc4
Carson McManus <carson.mcmanus1@gmail.com>
2017-01-17 18:35:08 -0500

    added speech/ and speech/base.py

---
+++ speech/base.py
@@ -0,0 +1,11 @@
+
+class BaseSpeechRecognizer(object):
+	"""docstring for BaseSpeechRecognizer."""
+	def __init__(self):
+		super(BaseSpeechRecognizer, self).__init__()
+
+	def Start():
+		pass
+
+	def Stop():
+		pass
