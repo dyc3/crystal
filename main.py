cf345cd6616ff29cc10797efdd9abf5ce0146f2c
Carson McManus <carson.mcmanus1@gmail.com>
2017-01-17 19:00:12 -0500

    moved training statements up a few lines

--- main.py
+++ main.py
@@ -10,13 +10,12 @@ nlp = spacy.load('en')
 cmdClassifier = classifier.CommandClassifier(nlp)
 
 train, labelsTrain = DataUtil.loadTrainingData("training.txt")
+print("Training...")
+cmdClassifier.fit(train, labelsTrain)
 
 test = ["give me the time", "is today monday", "give me the date", "what time is it on the east coast in military time",
 		"what time is it in pacific standard time", "pause music", "play the video", "stop playing that"]
 labelsTest = ["time", "date", "date", "time", "time", "media-pause", "media-play", "media-pause"]
-
-print("Training...")
-cmdClassifier.fit(train, labelsTrain)
 
 print("Testing...")
 preds = cmdClassifier.predict(test)
