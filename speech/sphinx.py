d3ef6b0758bcd28ecf43ef34669b4a4a1bf52b93
Carson McManus <carson.mcmanus1@gmail.com>
2017-01-19 11:54:28 -0500

    moved self.speech definition into Start()

--- speech/sphinx.py
+++ speech/sphinx.py
@@ -6,31 +6,33 @@ from pocketsphinx import LiveSpeech, get_model_path
 
 class SphinxSpeechRecognizer(BaseSpeechRecognizer):
 	"""docstring for SphinxSpeechRecognizer."""
-	def __init__(self, rate=44100, buffer_size=2048):
+	def __init__(self, rate=44100, buffer_size=2048, custom_model=false):
 		super(BaseSpeechRecognizer, self).__init__()
 		self.onSpeech = EventHook() # called while speaking, but not finished with request (only usable with speech streaming)
 		self.onFinish = EventHook() # called when speaking is finished
 		self.isRunning = False
 		self.status = "not-speaking"
 		self._notSpeakingTicks = 0
-		self.modal_path = get_model_path()
-
-		self.speech = LiveSpeech(
-			verbose=False,
-			sampling_rate=rate,
-			buffer_size=buffer_size,
-			no_search=False,
-			full_utt=False,
-			hmm=os.path.join(model_path, 'en-us'),
-			lm=os.path.join(model_path, 'en-us.lm.bin'),
-			dic=os.path.join(model_path, 'cmudict-en-us.dict')
-		)
+		if custom_model:
+			self.model_path = get_model_path()
 
 	def Start(self):
-		pass
+		if not self.isRunning:
+			self.speech = LiveSpeech(
+				verbose=False,
+				sampling_rate=rate,
+				buffer_size=buffer_size,
+				no_search=False,
+				full_utt=False,
+				hmm=os.path.join(model_path, 'en-us'),
+				lm=os.path.join(model_path, 'en-us.lm.bin'),
+				dic=os.path.join(model_path, 'cmudict-en-us.dict')
+			)
+			self.isRunning = True
 
 	def Stop(self):
-		pass
+		if self.isRunning:
+			self.isRunning = False
 
 	def GiveFrame(self, frame, speaking_power=200):
 		# TODO: finish this
