545c3fd1c5da7d7a9ff0c26973a26bbbd2dc3735
Carson McManus <carson.mcmanus1@gmail.com>
2017-01-20 14:20:53 -0500

    added get_flac_converter()

--- audio.py
+++ audio.py
@@ -3,6 +3,7 @@ import wave
 import threading
 import numpy
 import audioop
+import shutil
 from eventhook import EventHook
 
 class MicrophoneInput(object):
@@ -76,3 +77,28 @@ class MicrophoneInput(object):
 
 		stream.stop_stream()
 		stream.close()
+
+	def get_flac_converter():
+		"""Returns the absolute path of a FLAC converter executable, or raises an OSError if none can be found."""
+		flac_converter = shutil.which("flac")  # check for installed version first
+		if flac_converter is None:  # flac utility is not installed
+			base_path = os.path.dirname(os.path.abspath(__file__))  # directory of the current module file, where all the FLAC bundled binaries are stored
+			system, machine = platform.system(), platform.machine()
+			if system == "Windows" and machine in {"i686", "i786", "x86", "x86_64", "AMD64"}:
+				flac_converter = os.path.join(base_path, "flac-win32.exe")
+			elif system == "Darwin" and machine in {"i686", "i786", "x86", "x86_64", "AMD64"}:
+				flac_converter = os.path.join(base_path, "flac-mac")
+			elif system == "Linux" and machine in {"i686", "i786", "x86"}:
+				flac_converter = os.path.join(base_path, "flac-linux-x86")
+			elif system == "Linux" and machine in {"x86_64", "AMD64"}:
+				flac_converter = os.path.join(base_path, "flac-linux-x86_64")
+			else:  # no FLAC converter available
+				raise OSError("FLAC conversion utility not available - consider installing the FLAC command line application by running `apt-get install flac` or your operating system's equivalent")
+
+		# mark FLAC converter as executable if possible
+		try:
+			stat_info = os.stat(flac_converter)
+			os.chmod(flac_converter, stat_info.st_mode | stat.S_IEXEC)
+		except OSError: pass
+
+		return flac_converter
