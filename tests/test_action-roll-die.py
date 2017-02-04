c49ceecc2afc70c401621d1d96515c29c5560154
Carson McManus <carson.mcmanus1@gmail.com>
2017-02-04 15:19:20 -0500

    added test for action roll-die.rollDie()

---
+++ tests/test_action-roll-die.py
@@ -0,0 +1,14 @@
+import unittest
+from actions import roll_die
+
+class TestActionRollDie(unittest.TestCase):
+	"""docstring for TestActionRollDie."""
+
+	def test_roll_die(self):
+		result = roll_die.rollDie(6)
+		assert isinstance(result, list)
+		assert len(result) == 1
+		assert result[0] >= 1 and result[0] <= 6
+
+if __name__ == '__main__':
+	unittest.main()
