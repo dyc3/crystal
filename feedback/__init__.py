f1d056ff55ebdb1cf2111a6b1af411c771325696
Carson McManus <carson.mcmanus1@gmail.com>
2017-05-17 21:38:33 -0400

    import blink1

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -3,3 +3,4 @@ pgi.install_as_gi()
 import gi
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify # see: http://www.devdungeon.com/content/desktop-notifications-python-libnotify
+from blink1 import blink1
