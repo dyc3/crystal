6fb30b27989feec3bbb40ed2e228bfafb05efd24
Carson McManus <carson.mcmanus1@gmail.com>
2017-05-17 22:01:19 -0400

    add OnStatus

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -4,3 +4,15 @@ import gi
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify # see: http://www.devdungeon.com/content/desktop-notifications-python-libnotify
 from blink1 import blink1
+
+b1 = blink1.Blink1()
+
+def OnStatus(status):
+	if status == "idle":
+		b1.fade_to_color(100, "purple")
+	elif status == "listening":
+		b1.fade_to_color(100, "blue")
+	elif status == "working":
+		b1.fade_to_color(100, "orange")
+	elif status == "error":
+		b1.fade_to_color(100, "red")
