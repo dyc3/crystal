ac235c8f50142181c29eb22bcfbecb93917f197a
Carson McManus <carson.mcmanus1@gmail.com>
2017-05-17 21:11:27 -0400

    add feedback module

---
+++ feedback/__init__.py
@@ -0,0 +1,5 @@
+import pgi
+pgi.install_as_gi()
+import gi
+gi.require_version('Notify', '0.7')
+from gi.repository import Notify # see: http://www.devdungeon.com/content/desktop-notifications-python-libnotify
