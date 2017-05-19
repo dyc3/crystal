cfb43036ca009e8752cb8d9fe204ad4078f8c12e
Carson McManus <carson.mcmanus1@gmail.com>
2017-05-19 07:44:07 -0400

    adjust isSpeakingToCrystal()

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def isSpeakingToCrystal(doc):
 	sent = next(doc.sents)
 	for token in sent:
 		print(token, token.pos_, token.dep_, "parent:", token.head)
-		if token.dep_ in ["npadvmod", "ccomp", "nsubj"] and token.pos_ in ["NNP", "NN", "PROPN", "NOUN"] and str(token).lower() == "crystal":
+		if token.dep_ in ["npadvmod", "ccomp", "nsubj", "dobj"] and token.pos_ in ["NNP", "NN", "PROPN", "NOUN"] and str(token).lower() == "crystal":
 			return True
 	return False
 
