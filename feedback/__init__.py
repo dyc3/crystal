ab745c2cda8328ff6889f7b6eb408c110e8600ee
Carson McManus <carson.mcmanus1@gmail.com>
2017-05-19 22:49:20 -0400

    add check for blink1

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -5,14 +5,20 @@ gi.require_version('Notify', '0.7')
 from gi.repository import Notify # see: http://www.devdungeon.com/content/desktop-notifications-python-libnotify
 from blink1 import blink1
 
-b1 = blink1.Blink1()
+enableBlink1 = True
+try:
+	b1 = blink1.Blink1()
+except blink1.BlinkConnectionFailed as e:
+	print(e)
+	enableBlink1 = False
 
 def OnStatus(status):
-	if status == "idle":
-		b1.fade_to_color(100, "purple")
-	elif status == "listening":
-		b1.fade_to_color(100, "blue")
-	elif status == "working":
-		b1.fade_to_color(100, "orange")
-	elif status == "error":
-		b1.fade_to_color(100, "red")
+	if enableBlink1:
+		if status == "idle":
+			b1.fade_to_color(100, "purple")
+		elif status == "listening":
+			b1.fade_to_color(100, "blue")
+		elif status == "working":
+			b1.fade_to_color(100, "orange")
+		elif status == "error":
+			b1.fade_to_color(100, "red")
