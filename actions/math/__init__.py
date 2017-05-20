cd2b576cbe6af6fbe4c98e231620e1096b0552db
Carson McManus <carson.mcmanus1@gmail.com>
2017-05-19 22:36:30 -0400

    add math action, start implementation

---
+++ actions/math/__init__.py
@@ -0,0 +1,49 @@
+from actions import BaseAction
+
+def text2int(textnum, numwords={}):
+	if not numwords:
+		units = [
+			"zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+			"nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+			"sixteen", "seventeen", "eighteen", "nineteen",
+		]
+
+		tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+		scales = ["hundred", "thousand", "million", "billion", "trillion"]
+
+		numwords["and"] = (1, 0)
+		for idx, word in enumerate(units):	numwords[word] = (1, idx)
+		for idx, word in enumerate(tens):	 numwords[word] = (1, idx * 10)
+		for idx, word in enumerate(scales):   numwords[word] = (10 ** (idx * 3 or 2), 0)
+
+	current = result = 0
+	for word in textnum.split():
+		if word not in numwords:
+			raise Exception("Illegal word: " + word)
+
+		scale, increment = numwords[word]
+		current = current * scale + increment
+		if scale > 100:
+			result += current
+			current = 0
+
+	return result + current
+
+class ActionMath(BaseAction):
+	"""docstring for ActionMath."""
+	def __init__(self):
+		super(ActionMath, self).__init__()
+		self.handled_classifier = "math"
+
+	@classmethod
+	def run(self, doc):
+		sentence = next(doc.sents)
+		expression = ""
+		for token in sentence:
+			if token.pos_ == "NUM":
+				expression += str(text2int(str(token)))
+			
+
+def getAction():
+	return ActionMath()
