5095c1ddb0559bea81f3775436e15b19a290f99f
Carson McManus <carson.mcmanus1@gmail.com>
2017-05-22 09:54:49 -0400

    move date verify to seperate function for easier testing

--- actions/date/__init__.py
+++ actions/date/__init__.py
@@ -12,6 +12,40 @@ class ActionDate(BaseAction):
 		self.handled_classifier = "date"
 
 	@classmethod
+	def verify(self, sentence):
+		target_date = None
+		compare_date = None
+
+		# parse what we are looking in
+		target_token = [word for word in sentence[sentence.root.i:] if word.ent_type_ == "DATE"][0]
+		parse_string = str(target_token)
+		print("parsing target date: {}".format(parse_string))
+		time_struct, parse_status = cal.parse(parse_string)
+		if parse_status != 0:
+			target_date = datetime.datetime(*time_struct[:6])
+		else:
+			print("parse_status: {}".format(parse_status))
+
+		# parse what we are looking for
+		parse_string = str([word for word in sentence[sentence.root.i:] if word != target_token])
+		print("parsing compare date: {}".format(parse_string))
+		# ok, I'll admit this is really janky
+		context_struct = None
+		if target_date.date() == datetime.date.today() + datetime.timedelta(days=1):
+			context_struct, _ = cal.parse("today")
+		else:
+			context_struct, _ = cal.parse("last week")
+		print("context: ", context_struct)
+		time_struct, parse_status = cal.parse(parse_string, sourceTime=context_struct)
+		if parse_status != 0:
+			compare_date = datetime.datetime(*time_struct[:6])
+		else:
+			print("parse_status: {}".format(parse_status))
+
+		print("comparing dates - target: {}, compare: {}".format(target_date, compare_date))
+		return compare_date.date() == target_date.date()
+
+	@classmethod
 	def run(self, doc):
 		# print("Date: ", datetime.datetime.now().date().isoformat())
 		sentence = next(doc.sents)
@@ -36,30 +70,7 @@ class ActionDate(BaseAction):
 			print("Date: ", target_date.date().isoformat())
 			feedback.ShowNotify("Date: {}".format(target_date.date().isoformat()))
 		elif query_type == "verify":
-			compare_date = None
-
-			# parse what we are looking in
-			target_token = [word for word in sentence[sentence.root.i:] if word.ent_type_ == "DATE"][0]
-			parse_string = str(target_token)
-			print("parsing date: {}".format(parse_string))
-			time_struct, parse_status = cal.parse(parse_string)
-			if parse_status != 0:
-				target_date = datetime.datetime(*time_struct[:6])
-			else:
-				print("parse_status: {}".format(parse_status))
-
-			# parse what we are looking for
-			parse_string = str([word for word in sentence[sentence.root.i:] if word != target_token])
-			print("parsing date: {}".format(parse_string))
-			yesterday_struct, _ = cal.parse("last week")
-			time_struct, parse_status = cal.parse(parse_string, sourceTime=yesterday_struct)
-			if parse_status != 0:
-				compare_date = datetime.datetime(*time_struct[:6])
-			else:
-				print("parse_status: {}".format(parse_status))
-
-			print("comparing dates - target: {}, compare: {}".format(target_date, compare_date))
-			if compare_date.date() == target_date.date():
+			if self.verify(sentence):
 				feedback.ShowNotify("Yes")
 			else:
 				feedback.ShowNotify("No")
