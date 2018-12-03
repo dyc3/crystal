47432ef3d630f50773057ebfbb73ec9a3dbf08a5
Carson McManus <carson.mcmanus1@gmail.com>
2018-12-03 08:52:03 -0500

    remove unused import

--- crystal/feedback/__init__.py
+++ crystal/feedback/__init__.py
@@ -6,7 +6,6 @@ pgi.install_as_gi()
 import gi
 gi.require_version('Notify', '0.7')
 from gi.repository import Notify # see: http://www.devdungeon.com/content/desktop-notifications-python-libnotify
-from blink1 import blink1
 
 import crystal.core
 
