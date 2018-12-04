36bf3135776c124f3820b6cf09b443ede1f6923b
Carson McManus <carson.mcmanus1@gmail.com>
2018-12-03 21:03:05 -0500

    fix manipulate-wm desktop switching

--- crystal/actions/manipulate_wm/__init__.py
+++ crystal/actions/manipulate_wm/__init__.py
@@ -8,41 +8,28 @@ class ActionManipulateWm(BaseAction):
 		self.handled_classifier = "manipulate-wm"
 
 	@classmethod
+	def parse(self, sentence):
+		"""
+		Returns a i3-msg command with arguments to complete the action.
+		"""
+		command = None
+		if str(sentence.root) in ["switch", "focus", "show", "pull", "go"]:
+			for word in sentence:
+				if word.lemma_ in ["workspace", "space", "desktop"]:
+					num_token = word.nbor(1)
+					try:
+						num = int(str(num_token))
+					except:
+						num = utils.text2int(str(num_token).lower())
+					command = 'i3-msg "workspace {}"'.format(num)
+
+		return command
+
+	@classmethod
 	def run(self, doc):
 		sentence = next(doc.sents)
 		utils.printSpacy(sentence)
-		command = None
-		workspace_alias = ["workspace", "space", "desktop"]
-		if str(sentence.root) in ["switch", "focus", "show", "pull", "go"]:
-			dobj_token = None
-			for child in sentence.root.children:
-				if str(child).lower() in workspace_alias:
-					if child.dep_ == "dobj":
-						dobj_token = child
-					elif child.dep_ == "prep":
-						for c in child.children:
-							if c.dep_== "pobj":
-								dobj_token = c
-					elif child.dep_ == "ccomp":
-						dobj_token = child
-			if dobj_token == None:
-				print("Unsupported window manager manipulation")
-			elif str(dobj_token).lower() in workspace_alias:
-				command = None
-				for child in dobj_token.children:
-					if child.like_num:
-						try:
-							num = int(str(child))
-						except Exception as e:
-							num = utils.text2int(str(child))
-						command = 'i3-msg "workspace {}"'.format(num)
-			elif str(dobj_token).lower() in ["window"]:
-				print("TODO: focus windows")
-			else:
-				print("Unsupported window manager manipulation: dobj_token: {}".format(dobj_token))
-		elif str(sentence.root) in ["left", "right"]:
-			print("TODO: Unsupported window manager manipulation: {}".format(sentence.root))
-			print('Try something like "Focus left window"')
+		command = self.parse(sentence)
 
 		if command != None:
 			exitcode = utils.runAndPrint(command)
