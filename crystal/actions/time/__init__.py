547eeb0af5e88a95abb08939071cad4ce7ea6f4d
Carson McManus <carson.mcmanus1@gmail.com>
2018-12-10 11:00:39 -0500

    set up time action to add more features later, add test for time action

--- crystal/actions/time/__init__.py
+++ crystal/actions/time/__init__.py
@@ -9,9 +9,24 @@ class ActionTime(BaseAction):
 		self.handled_classifier = "time"
 
 	@classmethod
+	def parse(self, sentence):
+		# TODO: implement grabing time from different timezones
+		context_timezone = None # if None, use system's timezone
+		target_timezone = None # if None, get the system time
+		target_time = None # if None, get the current time
+
+		return context_timezone, target_timezone, target_time
+
+	@classmethod
 	def run(self, doc):
-		print("Time: ", datetime.datetime.now().time().isoformat())
-		feedback.ShowNotify("Time: {}".format(datetime.datetime.now().time().isoformat()))
+		sentence = next(doc.sents)
+		context_timezone, target_timezone, target_time = self.parse(sentence)
+
+		current_time = datetime.datetime.now().time()
+		result = "Time: {}".format(current_time.isoformat())
+		print(result)
+		feedback.ShowNotify(result)
+		return result
 
 def getAction():
 	return ActionTime()
---
+++ tests/test_action-time.py
@@ -0,0 +1,36 @@
+import unittest
+import spacy
+from crystal.actions import time
+
+nlp = None
+
+class TestActionTime(unittest.TestCase):
+	"""docstring for TestActionTime."""
+
+	def setUp(self):
+		global nlp
+		nlp = spacy.load("en")
+
+	def test_time_parse(self):
+		# query, context timezone, expected timezone, expected time
+		test_set = [
+			("what is the time", None, None, None),
+			("what's the time", None, None, None),
+			("what time is it", None, None, None),
+			("give me the time", None, None, None),
+
+			# ("what is the time on the east coast", None, -5, None),
+			# ("what is the time on the west coast", None, -8, None),
+		]
+		action_time = time.ActionTime()
+		for test, expected_context_timezone, expected_timezone, expected_time in test_set:
+			doc = nlp(test)
+			sent = next(doc.sents)
+
+			result_context_timezone, result_timezone, result_time = action_time.parse(sent)
+			self.assertEqual(result_context_timezone, expected_context_timezone)
+			self.assertEqual(result_timezone, expected_timezone)
+			self.assertEqual(result_time, expected_time)
+
+if __name__ == '__main__':
+	unittest.main()
