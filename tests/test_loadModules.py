fa209ce256c525d1c727ee3ae0067011b91fed81
Carson McManus <carson.mcmanus1@gmail.com>
2019-01-28 11:59:39 -0500

    add test_check_actions_for_duplicate_handlers

--- tests/test_loadModules.py
+++ tests/test_loadModules.py
@@ -7,6 +7,16 @@ class TestLoadModules(unittest.TestCase):
 	def test_loadActions(self):
 		actions.load_actions()
 
+	def test_check_actions_for_duplicate_handlers(self):
+		"""
+		Checks handled_classifier in each action module for duplicates.
+		"""
+		action_modules = actions.load_actions()
+		seen_handlers = []
+		for a in action_modules:
+			self.assertFalse(a in seen_handlers)
+			seen_handlers.append(a)
+
 	def test_loadFeedback(self):
 		feedback.load_feedback()
 
