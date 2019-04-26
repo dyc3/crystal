b00633f139c80e65bd9c19d74d52c1e45a52d48c
Carson McManus <carson.mcmanus1@gmail.com>
2019-04-26 12:46:45 -0400

    add more parse tests for media action

--- tests/test_action_media.py
+++ tests/test_action_media.py
@@ -20,29 +20,36 @@ class TestActionMedia(unittest.TestCase):
 			("play the video", "play"),
 			("resume playback", "play"),
 			("resume media playback", "play"),
+			("crystal play music", "play"),
 
 			("pause music", "pause"),
 			("pause playback", "pause"),
 			("pause media playback", "pause"),
 			("hold playback", "pause"),
+			("crystal pause the music", "pause"),
 
 			("skip this song", "next"),
 			("next song", "next"),
+			("crystal skip", "next"),
 
 			("previous song", "previous"),
 			("previous video", "previous"),
+			("crystal go back to the previous song", "previous"),
 
 			("go to 8 seconds", "position 8"),
 			("seek position to 2 minutes", "position 120"),
 			("go to 2 minutes 30 seconds", "position 150"),
 			("go to 2 minutes and 30 seconds", "position 150"),
+			("crystal go to 3 minutes and 45 seconds", "position 225"),
 
 			("skip ahead 4 seconds", "position +4"),
 			("go forward 1 minute", "position +60"),
+			("crystal skip ahead 2 minutes and 10 seconds", "position +130"),
 
 			("skip back 30 seconds", "position -30"),
 			("skip back 1 minute and 30 seconds", "position -90"),
 			("go back 4 seconds", "position -4"),
+			("crystal go back 12 seconds", "position -12"),
 		]
 		action_media = media.ActionMedia()
 		for test, expectedResult in test_set:
