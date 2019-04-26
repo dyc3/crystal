d0661aec2d8856e4358bd04f4cd602fe796dd297
Carson McManus <carson.mcmanus1@gmail.com>
2019-04-26 12:34:12 -0400

    add parse tests for weather

---
+++ tests/test_action_weather.py
@@ -0,0 +1,32 @@
+import unittest
+import datetime
+import spacy
+from crystal.actions import weather
+
+nlp = None
+
+class TestActionWeather(unittest.TestCase):
+	"""docstring for TestActionWeather."""
+
+	@classmethod
+	def setUpClass(self):
+		global nlp
+		nlp = spacy.load("en")
+
+	def test_parse(self):
+		# query, expected result
+		test_set = [
+			("what's the weather", "current"),
+			("how hot is it", "current"),
+			("is it cold outside", "current"),
+			("do I need a jacket", "current"),
+		]
+		action_weather = weather.ActionWeather()
+		for test, expectedResult in test_set:
+			with self.subTest():
+				doc = nlp(test)
+				sent = next(doc.sents)
+				self.assertEqual(action_weather.parse(sent), expectedResult, test)
+
+if __name__ == '__main__':
+	unittest.main()
