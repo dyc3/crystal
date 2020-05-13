605f29905f9b77ce715492b0b979a687c5ba8020
Carson McManus <carson.mcmanus1@gmail.com>
2020-05-13 15:59:37 -0400

    add bare minimum smart-home action

---
+++ crystal/actions/smart_home/__init__.py
@@ -0,0 +1,77 @@
+from crystal.actions import BaseAction
+from crystal.actions.responses import *
+import pywemo
+import datetime
+from crystal import feedback
+import utils
+import logging
+log = logging.getLogger(__name__)
+
+class ActionSmartHome(BaseAction):
+	"""docstring for ActionSmartHome."""
+	def __init__(self):
+		super(ActionSmartHome, self).__init__()
+		self.handled_classifier = "smart-home"
+		self.requires_updater = True
+		self.init()
+
+	@classmethod
+	def init(self):
+		self.devices = []
+		self.last_device_scan = datetime.datetime.now() - datetime.timedelta(seconds=5*60)
+
+	@classmethod
+	def parse(self, doc):
+		verb_token = utils.find_word(doc, ["toggle", "turn"])
+		if not verb_token:
+			raise Exception("Unable to find verb")
+		target_token = None
+		for token in doc[verb_token.i + 1:]:
+			if token.pos_ == "NOUN":
+				target_token = token
+				break
+		if not target_token:
+			raise Exception("Unable to find target")
+
+		if verb_token.text == "toggle":
+			objective_state = "toggle"
+		else:
+			objective_state = 1 if verb_token.nbor(1).text == "on" else 0
+
+		device_name = target_token.text
+
+		return device_name, objective_state
+
+	@classmethod
+	def run(self, doc):
+		device_name, objective_state = self.parse(doc)
+		log.info(f"Set {device_name}: {objective_state}")
+		target_device = None
+		for device in self.devices:
+			if device_name in device.name.lower():
+				target_device = device
+				break
+		log.info(f"Selected: {target_device}")
+		if objective_state == "toggle":
+			target_device.toggle()
+		else:
+			target_device.set_state(objective_state)
+		return ActionResponseBasic(ActionResponseType.SUCCESS)
+
+	@classmethod
+	def update(self):
+		delta = datetime.datetime.now() - self.last_device_scan
+		if delta.total_seconds() < 5 * 60:
+			return
+
+		self.devices = pywemo.discover_devices()
+		self.devices += [
+			pywemo.discovery.device_from_description(f'http://192.168.0.10:{pywemo.ouimeaux_device.probe_wemo("192.168.0.10")}/setup.xml', None),
+			pywemo.discovery.device_from_description(f'http://192.168.0.23:{pywemo.ouimeaux_device.probe_wemo("192.168.0.23")}/setup.xml', None),
+		]
+		# self.devices[0].toggle()
+		log.info(f"found {len(self.devices)} devices")
+		self.last_device_scan = datetime.datetime.now()
+
+def getAction():
+	return ActionSmartHome()
