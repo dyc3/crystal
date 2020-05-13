cff10420e654f6701ef883500dee2dbd92a7d0b0
Carson McManus <carson.mcmanus1@gmail.com>
2020-05-13 13:26:18 -0400

    play pattern on recording finish

--- crystal/feedback/blink1/__init__.py
+++ crystal/feedback/blink1/__init__.py
@@ -1,6 +1,7 @@
 import crystal.core
 from crystal.core import CrystalStatus
 from blink1 import blink1
+import usb.core
 import logging
 log = logging.getLogger(__name__)
 
@@ -21,6 +22,10 @@ def on_status_update(status):
 def on_core_exit():
 	b1.fade_to_color(2000, "black")
 
+def on_recording_finish(raw_audio: bytes, sample_rate: int, sample_width: int):
+	pattern_str = '0, blue,0.3,1, orange,0.1,2,  black,0.3,2'
+	b1.play_pattern(pattern_str)
+
 def register():
 	global b1
 	try:
@@ -31,8 +36,10 @@ def register():
 
 	crystal.core.on_status_update += on_status_update
 	crystal.core.on_core_exit += on_core_exit
+	crystal.core.on_recording_finish += on_recording_finish
 
 def unregister():
 	if b1:
 		crystal.core.on_status_update -= on_status_update
 		crystal.core.on_core_exit -= on_core_exit
+		crystal.core.on_recording_finish -= on_recording_finish
