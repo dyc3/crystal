e2487aa3cdf0f3ab4170054e604bcd45d3e1c138
Carson McManus <carson.mcmanus1@gmail.com>
2020-05-14 12:37:23 -0400

    smart-home: more robust device selection

--- crystal/actions/smart_home/__init__.py
+++ crystal/actions/smart_home/__init__.py
@@ -4,6 +4,8 @@ import pywemo
 import datetime
 from crystal import feedback
 import utils
+import crystal.core.processing
+from spacy.tokens import Doc
 import logging
 log = logging.getLogger(__name__)
 
@@ -11,13 +13,20 @@ class DeviceWrapper(object):
 	def __init__(self, device):
 		super(DeviceWrapper, self).__init__()
 		self.internal_device = device
+		self._name_processed: Doc = None
 
 	@property
-	def name(self):
+	def name(self) -> str:
 		if isinstance(self.internal_device, dict):
 			return self.internal_device["name"]
 		else:
 			return self.internal_device.name
+
+	@property
+	def name_processed(self) -> Doc:
+		if not self._name_processed:
+			self._name_processed = crystal.core.processing.parse_nlp(self.name)
+		return self._name_processed
 
 	def set_state(self, state):
 		self.internal_device.set_state(state)
@@ -33,19 +42,16 @@ class ActionSmartHome(BaseAction):
 		self.requires_updater = True
 		self.init()
 
-	@classmethod
 	def init(self):
 		self.devices = []
 		self.last_device_scan = datetime.datetime.now() - datetime.timedelta(seconds=5*60)
 
-	@classmethod
 	def get_query_type(self, doc):
 		if utils.find_word(doc, ["toggle", "turn"]):
 			return "interact"
 		if utils.find_word(doc, ["scan", "look"]):
 			return "scan"
 
-	@classmethod
 	def parse_interact(self, doc):
 		verb_token = utils.find_word(doc, ["toggle", "turn"])
 		if not verb_token:
@@ -75,24 +81,33 @@ class ActionSmartHome(BaseAction):
 
 		return device_name, objective_state
 
-	@classmethod
 	def scan_for_devices(self):
 		self.devices = pywemo.discover_devices()
+		# HACK: because discover devices doesn't work
+		# we should just do our own portscan.
 		self.devices += [
 			pywemo.discovery.device_from_description(f'http://192.168.0.10:{pywemo.ouimeaux_device.probe_wemo("192.168.0.10")}/setup.xml', None),
 			pywemo.discovery.device_from_description(f'http://192.168.0.23:{pywemo.ouimeaux_device.probe_wemo("192.168.0.23")}/setup.xml', None),
+			pywemo.discovery.device_from_description(f'http://192.168.0.21:{pywemo.ouimeaux_device.probe_wemo("192.168.0.21")}/setup.xml', None),
 		]
 		self.devices = list([DeviceWrapper(d) for d in self.devices])
 		log.info(f"found {len(self.devices)} devices")
 		self.last_device_scan = datetime.datetime.now()
 
-	@classmethod
-	def select_device(self, name: str):
-		for device in self.devices:
-			if name in device.name.lower():
-				return device
+	def select_device(self, name: str) -> DeviceWrapper:
+		name_doc = crystal.core.processing.parse_nlp(name)
+		scores = {}
+		for idx, device in enumerate(self.devices):
+			score = name_doc.similarity(device.name_processed)
+			# HACK: boost scores of items in carson's room
+			# To fix this, Crystal needs to know what room the request is coming from,
+			# and what room each device is located in
+			if "carson" in device.name.lower():
+				score += 0.075
+			log.debug(f"score: {name} <-> {device.name} == {score}")
+			scores[idx] = score
+		return self.devices[max(scores, key=scores.get)]
 
-	@classmethod
 	def run(self, doc):
 		query_type = self.get_query_type(doc)
 		if query_type == "interact":
@@ -111,7 +126,6 @@ class ActionSmartHome(BaseAction):
 		else:
 			return ActionResponseBasic(ActionResponseType.FAILURE, f"Unknown query type: {query_type}")
 
-	@classmethod
 	def update(self):
 		delta = datetime.datetime.now() - self.last_device_scan
 		if delta.total_seconds() < 5 * 60:
