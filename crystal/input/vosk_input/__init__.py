e90ee8f83aa4100eb591726c8af28120268eecce
“Carson <carson.mcmanus1@gmail.com>
2020-10-14 08:41:43 -0400

    add more vosk input auto corrects

--- crystal/input/vosk_input/__init__.py
+++ crystal/input/vosk_input/__init__.py
@@ -55,15 +55,21 @@ class VoskInput(BaseInput):
 		# HACK: auto correct text to match domain vocabulary. Sorry.
 		full_text = full_text.replace("palace music", "pause music")
 		full_text = full_text.replace("applause music", "pause music")
-		if "turn on" in full_text or "turn off" in full_text:
+		if any(x in full_text for x in ["turn on", "turn off", "turned on", "turned off"]):
 			full_text = full_text.replace("the land", "the lamp").replace("the lamb", "the lamp")
 			if full_text.endswith("the lam"):
 				full_text = full_text.replace("the lam", "the lamp")
+		if any(x in full_text for x in ["timer", "alarm"]):
+			full_text = full_text.replace("crystal said", "crystal set")
 		if full_text.endswith("to pm"):
 			full_text = full_text.replace("to pm", "2 pm")
-		if full_text.endswith(" a m"):
+		elif full_text.endswith(" a m"):
 			full_text = full_text.replace(" a m", " am")
 		if full_text.startswith("christo"):
 			full_text = full_text.replace("christo", "crystal")
+		elif full_text.startswith("crews to"):
+			full_text = full_text.replace("crews to", "crystal")
+		elif full_text.startswith("christian"):
+			full_text = full_text.replace("christian", "crystal")
 
 		return full_text
